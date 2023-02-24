4ad92b9f59b370739441e1b217739a2b2670424b
Silas Castro <silascastro15@gmail.com>
2023-02-24 11:06:16 -0400

    updating angular to v12

--- frontend/lgapp/src/app/shared/new-product-modal/new-product-modal.component.spec.ts
+++ frontend/lgapp/src/app/shared/new-product-modal/new-product-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NewProductModalComponent } from './new-product-modal.component';
 
@@ -6,7 +6,7 @@ describe('NewProductModalComponent', () => {
   let component: NewProductModalComponent;
   let fixture: ComponentFixture<NewProductModalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NewProductModalComponent ]
     })
